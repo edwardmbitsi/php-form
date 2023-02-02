8da64663f8f76c70abee96bb86998259514cd21a
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2023-02-02 19:42:30 +0300

    Update form.php

--- form.php
+++ form.php
@@ -14,7 +14,7 @@
     $dbname = "testing";
 
    // Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','testing');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
